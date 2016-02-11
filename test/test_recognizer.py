dc9d146beeb95e0b0eb906aa7620601551a4e6e1
HappyTatsuhito <b1730096@planet.kanazawa-it.ac.jp>
2016-02-12 03:37:11 +0900

    testのデバッグ完了 21/3/10 by Jetson

--- test/test_recognizer.py
+++ test/test_recognizer.py
@@ -3,7 +3,12 @@
 
 import sys
 import rospy
+<<<<<<< HEAD
 from mimi_manipulation_pkg.srv import RecognizeCount, RecognizeFind, RecognizeLocalize
+=======
+# -- ros srvs --
+from mimi_manipulation_pkg.srv import RecognizeFind, RecognizeCount, RecognizeLocalize
+>>>>>>> 16e7e2b... testのデバッグ完了 21/3/10 by Jetson
 
 
 if __name__ == "__main__":
@@ -13,6 +18,7 @@ if __name__ == "__main__":
     rospy.init_node("test_recognizer")
 
     count_object = rospy.ServiceProxy("/recognize/count", RecognizeCount)
+<<<<<<< HEAD
     find_object = rospy.ServiceProxy("/recognize/find", RecognizeFind)
     localize_object = rospy.ServiceProxy("/recognize/localize", RecognizeLocalize)
 
@@ -32,3 +38,21 @@ if __name__ == "__main__":
     rospy.wait_for_service("/recog/localize")
     res = localize_object(target_name)
     rospy.loginfo("\nmodule : localize\n", res.centroid_point)
+=======
+    localize_object = rospy.ServiceProxy("/recognize/localize", RecognizeLocalize)
+
+    rospy.wait_for_service("/recognize/find")
+    res = find_object(target_name)
+    rospy.loginfo("Find Ojbect")
+    rospy.loginfo(res.result)
+
+    rospy.wait_for_service("/recognize/count")
+    res = count_object(target_name)
+    rospy.loginfo("Count Ojbect")
+    rospy.loginfo(res)
+
+    rospy.wait_for_service("/recognize/localize")
+    res = localize_object(target_name)
+    rospy.loginfo("Localize Ojbect")
+    rospy.loginfo(res)
+>>>>>>> 16e7e2b... testのデバッグ完了 21/3/10 by Jetson
