08625cbdd6adee00b63c257af2c423c2009615e8
HappyTatsuhito <b1730096@planet.kanazawa-it.ac.jp>
2020-10-08 17:44:15 +0900

    first commit by Laptop 10/8

--- src/new_object_recognizer.py
+++ src/new_object_recognizer.py
@@ -4,6 +4,10 @@
 import sys
 import time
 import math
+<<<<<<< HEAD
+=======
+import numpy
+>>>>>>> e485a97... first commit by Laptop 10/8
 import rospy
 import rosparam
 import actionlib
@@ -28,7 +32,11 @@ class MimiControl(object):
             degree = degree - 360
         while degree < -180:
             degree = degree + 360
+<<<<<<< HEAD
         angular_speed = 70.0 #[deg/s]
+=======
+        angular_speed = 90.0 #[deg/s]
+>>>>>>> e485a97... first commit by Laptop 10/8
         target_time = abs(1.76899*(degree /angular_speed))  #[s]
         if degree >= 0:
             self.twist_value.angular.z = (angular_speed * 3.14159263 / 180.0) #rad
@@ -38,11 +46,19 @@ class MimiControl(object):
         start_time = time.time()
         end_time = time.time()
         while end_time - start_time <= target_time:
+<<<<<<< HEAD
             self.cmd_vel_pub.publish(self.twist_value)
             end_time = time.time()
             rate.sleep()
         self.twist_value.angular.z = 0.0
         self.cmd_vel_pub.publish(self.twist_value)
+=======
+            self.pub_twist.publish(self.twist_value)
+            end_time = time.time()
+            rate.sleep()
+        self.twist_value.angular.z = 0.0
+        self.pub_twist.publish(self.twist_value)
+>>>>>>> e485a97... first commit by Laptop 10/8
 
     def moveBase(self, rad_speed):
         for speed_i in range(10):
@@ -96,6 +112,7 @@ class RecognizeTools(object):
             if time.time() - self.update_time > 1.5 and self.update_flg:
                 self.bbox = []
                 self.update_flg = False
+<<<<<<< HEAD
                 rospy.loginfo('initialize')
             rate.sleep()
 
@@ -137,6 +154,25 @@ class RecognizeTools(object):
             mimi_control.angleRotation(rotation_angle)
             rospy.sleep(3.0)
             bbox_list = self.createBboxList(self.bbox)
+=======
+                rospy.loginfo('initialize') # test
+            rate.sleep()
+
+    def findObject(self, object_name='None'):
+        rospy.loginfo("module type : Find")
+        mimi_control = MimiControl()
+        if type(object_name) != str:
+            object_name = object_name.target
+        find_flg = False
+        find_count = 0
+        while not find_flg and find_count <= 3 and not rospy.is_shutdown():
+            bbox_list = self.createBboxList(self.bbox)
+            #rotate
+            find_count += 1
+            rotation_angle = 45 - (((find_count)%4)/2) * 90
+            mimi_control.angleRotation(rotation_angle)
+            rospy.sleep(2.0)
+>>>>>>> e485a97... first commit by Laptop 10/8
             if object_name == 'None':
                 find_flg = bool(len(self.bbox))
             elif object_name == 'any':
@@ -146,32 +182,58 @@ class RecognizeTools(object):
         return find_flg
             
     def countObject(self, object_name='None', bb=None):
+<<<<<<< HEAD
         if bb is None:
             bb = self.bbox
         if type(object_name) != str:
             object_name = object_name.target_name
+=======
+        rospy.loginfo("module type : Count")
+        if bb is None:
+            bb = self.bbox
+        if type(object_name) != str:
+            object_name = object_name.target
+>>>>>>> e485a97... first commit by Laptop 10/8
         object_list = []
         bbox_list = self.createBboxList(bb)
         if object_name == 'any':
             any_dict = {}
+<<<<<<< HEAD
             for i in range(len(object_list)):
                 if object_list[i] in object_dict['any']:
                     any_dict[object_list[i]] = bb[i].xmin
+=======
+            for i in range(len(bbox_list)):
+                if bbox_list[i] in self.object_dict['any']:
+                    any_dict[bbox_list[i]] = bb[i].xmin
+>>>>>>> e485a97... first commit by Laptop 10/8
             sorted_any_dict = sorted(any_dict.items(), key=lambda x:x[1])
             for i in range(len(sorted_any_dict)):
                 object_list.append(sorted_any_dict[i][0])
             object_count = len(sorted_any_dict)
         else:
             object_count = bbox_list.count(object_name)
+<<<<<<< HEAD
             object_list = bbox_list
         return object_count, object_list
 
     def localizeObject(self, object_name, bb=None):
+=======
+            obejct_list = bbox_list
+        return object_count, object_list
+
+    def localizeObject(self, object_name, bb=None):
+        rospy.loginfo("module type : Localize")
+>>>>>>> e485a97... first commit by Laptop 10/8
         Detector = CallDetector()
         if bb is None:
             bb = self.bbox
         if type(object_name) != str:
+<<<<<<< HEAD
             object_name = object_name.target_name
+=======
+            object_name = object_name.target
+>>>>>>> e485a97... first commit by Laptop 10/8
         object_centroid = Point()
         bbox_list = self.createBboxList(bb)
         object_count, _ = self.countObject(object_name)
@@ -187,7 +249,11 @@ class RecognizeTools(object):
         image_range.bottom = bb[index_num].ymax
         image_range.left = bb[index_num].xmin
         image_range.right = bb[index_num].xmax
+<<<<<<< HEAD
         rospy.sleep(0.5)
+=======
+        rospy.sleep(0.2)
+>>>>>>> e485a97... first commit by Laptop 10/8
         Detector.image_range_pub.publish(image_range)
         while Detector.centroid_flg == False and not rospy.is_shutdown():
             pass
@@ -204,11 +270,18 @@ class RecognizeAction(object):
                                                 auto_start = False)
         self.act.register_preempt_callback(self.actionPreempt)
 
+<<<<<<< HEAD
         self.preempt_flg = False
         self.act.start()
 
         self.recognize_tools = RecognizeTools()
         self.recognize_tools.initializeBBox()
+=======
+        self.find_count = 0
+        self.move_count = 0
+
+        self.act.start()
+>>>>>>> e485a97... first commit by Laptop 10/8
 
     def actionPreempt(self):
         rospy.loginfo('preempt callback')
@@ -221,6 +294,7 @@ class RecognizeAction(object):
         action_feedback = ObjectRecognizerFeedback()
         action_result = ObjectRecognizerResult()
         mimi_control = MimiControl()
+<<<<<<< HEAD
         #recognize_tools = RecognizeTools()
         move_count = 0
         while not rospy.is_shutdown():
@@ -270,4 +344,61 @@ if __name__ == '__main__':
     rospy.init_node('object_recognizer')
     recognize_action = RecognizeAction()
     #recognize_action.recognize_tools.initializeBBox()
+=======
+        recognize_tools = RecognizeTools()
+        target_dict = recognize_tools.object_dict
+        loop_flg = True
+        while loop_flg and not rospy.is_shutdown():
+            bb = recognize_tools.bbox
+            if target_name in target_dict.keys():
+                for i in range(len(target_dict[target_name])):
+                    rospy.loginfo(target_dict[target_name][i])
+                    object_count, _ = recognize_tools.countObject(target_dict[target_name][i], bb)
+                    if bool(object_count):
+                        target_name = target_dict[target_name][i]
+                        break
+            else:
+                object_count, _ = recognize_tools.countObject(target_name, bb)
+            exist_flg = bool(object_count)
+            if exist_flg:
+                object_centroid = recognize_tools.localizeObject(target_name)
+                if not math.isnan(object_centroid.x):# 物体が正面になるように回転する処理
+                    object_centroid.y += 0.08 # calibrate RealSenseCamera d435
+                    object_angle = math.atan2(object_centroide.y, object_centroid.x)/math.pi*180
+                    if abs(object_angle) > 4.5:
+                        # retry
+                        rospy.loginfo('There is not object in front.')
+                        mimi_control.angleRotation(object_angle)
+                        rospy.sleep(4.0)
+                    else:
+                        # success
+                        loop_flg = False
+                else:
+                    #前後進
+                    pass
+            else:
+                find_flg = recognize_tools.findObject(target_name)
+                ###
+                # ここの続きを書く。
+                # 見つからなかったときにwhileを抜けるように
+                ###
+            if loop_flg:
+                action_feedback.recog_feedback = exist_flg
+                self.act.publish_feedback(action_feedback)
+            exist_flg = False
+            if self.preempt_flg:
+                self.preempt_flg = False
+                break
+        else:
+            rospy.loginfo('Succeeded')
+            action_result.recog_result = object_centroid
+            self.act.set_succeeded(action_result)
+                
+                
+if __name__ == '__main__':
+    rospy.init_node('object_recognizer')
+    recognize_tools = RecognizeTools()
+    recognize_tools.initializeBBox()
+    recognize_action = RecognizeAction()
+>>>>>>> e485a97... first commit by Laptop 10/8
     rospy.spin()
