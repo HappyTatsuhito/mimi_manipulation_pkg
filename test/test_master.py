7e6234cb3da1c484304fad83c33cc9ca43574669
HappyTatsuhito <b1730096@planet.kanazawa-it.ac.jp>
2020-12-18 16:47:00 +0900

    create testcode by Laptop 20/12/18

--- test/test_master.py
+++ test/test_master.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 
+import sys
 import rospy
 from mimi_manipulation_pkg.srv import ManipulateSrv
 
 
 if __name__ == "__main__":
+    args = sys.argv
+
     rospy.init_node("test_master")
-    print "ok"
+
+    manipulation_master = rospy.ServiceProxy("/manipulation", ManipulateSrv)
+
+    rospy.wait_for_service("/manipulation")
+    res = manipulation_master(args[-1])
+    rospy.loginfo("manipulation_master\n", res.result)
--- test/test_recognizer.py
+++ test/test_recognizer.py
@@ -1,2 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
+
+import sys
+import rospy
+from mimi_manipulation_pkg.srv import RecognizeCount, RecognizeFind, RecognizeLocalize
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    target_name = args[-1]
+
+    rospy.init_node("test_recognizer")
+
+    count_object = rospy.ServiceProxy("/recog/count", RecognizeCount)
+    find_object = rospy.ServiceProxy("/recog/find", RecognizeFind)
+    localize_object = rospy.ServiceProxy("/recog/localize", RecognizeLocalize)
+
+    rospy.wait_for_service("/recog/count")
+    res = count_object(target_name)
+    rospy.loginfo("module : count\n", res.object_num, res.object_list)
+    
+    rospy.wait_for_service("/recog/find")
+    res = find_object(target_name)
+    rospy.loginfo("\nmodule : find\n", res.result)
+
+    rospy.wait_for_service("/recog/localize")
+    res = localize_object(target_name)
+    rospy.loginfo("\nmodule : localize\n", res.centroid_point)
