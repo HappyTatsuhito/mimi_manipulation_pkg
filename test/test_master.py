e91ff74cca2c294b485ff12cf0ae63a1487e9aac
HappyTatsuhito <b1730096@planet.kanazawa-it.ac.jp>
2020-12-18 15:37:10 +0900

    テストコード用のtestディレクトリの作成 by Laptop 20/12/18

--- test/test_master.py
+++ test/test_master.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
 
-import sys
 import rospy
-# -- ros srvs --
 from mimi_manipulation_pkg.srv import ManipulateSrv
 
-if __name__ == '__main__':
-    args = sys.argv
-    target_name = args[-1]
-    
-    rospy.init_node('test_master')
 
-    manipulation_master = rospy.ServiceProxy("/manipulation", ManipulateSrv)
-
-    rospy.wait_for_service("/manipulation")
-    res = manipulation_master(target_name)
-    rospy.loginfo("manipulation_master\n", res.result)
+if __name__ == "__main__":
+    rospy.init_node("test_master")
+    print "ok"
--- test/test_recognizer.py
+++ test/test_recognizer.py
@@ -1,29 +1,2 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*
-
-import sys
-import rospy
-# -- ros srvs --
-from mimi_manipulation_pkg.srv import RecognizeCount, RecognizeFind, RecognizeLocalize
-
-if __name__ == '__main__':
-    args = sys.argv
-    target_name = args[-1]
-    
-    rospy.init_node('test_recognizer')
-
-    find_object = rospy.ServiceProxy("/recognize/find", RecognizeFind)
-    count_object = rospy.ServiceProxy("/recognize/count", RecognizeCount)
-    localize_ojbect = rospy.ServiceProxy("/recognize/localize", RecognizeLocalize)
-
-    rospy.wait_for_service("/recognize/find")
-    res = find_ojbect(target_name)
-    rospy.loginfo("module : find\n", res.result)
-
-    rospy.wait_for_service("/recognize/count")
-    res = count_ojbect(target_name)
-    rospy.loginfo("\nmodule : count\n", res.result)
-
-    rospy.wait_for_service("/recognize/localize")
-    res = localize_ojbect(target_name)
-    rospy.loginfo("\nmodule : localize\n", res.result)
